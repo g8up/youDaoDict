434115d5d7bad1bb72bd1f29d8ea7a1ee4fc3427
g8up <g8up@qq.com>
2015-06-15 11:35:04 +0800

    设置初始状态为开启

--- common.js
+++ common.js
@@ -1,8 +1,8 @@
 (function(win) {
 	var _isDev = !1;
 	var _DefaultOptions = {
+		"dict_enable": ["checked", true],
 		"ctrl_only": ["checked", false],
-		"dict_enable": ["checked", false],
 		"english_only": ["checked", true],
 		"history_count": 5
 	};
