2a8a8596bcb442c72e636d5040bfd75d94b062cc
g8up <g8up@qq.com>
2016-04-30 21:57:24 +0800

    option 页面添加朗读按钮

--- src/options.js
+++ src/options.js
@@ -112,7 +112,7 @@ function buildSearchResult() {
 		if (langType == 'ko') basetrans = "<strong>韩汉翻译:</strong><br/>" + basetrans;
 		else if (langType == 'jap') basetrans = "<strong>日汉翻译:</strong><br/>" + basetrans;
 		else if (langType == 'fr') basetrans = "<strong>法汉翻译:</strong><br/>" + basetrans;
-		else basetrans = "<strong>英汉翻译:</strong><br/>" + basetrans;
+		else basetrans = "<strong>英汉翻译:</strong><span class='word-speech' data-toggle='play'></span><br/>" + basetrans;
 		res.innerHTML = basetrans;
 	}
 	if (noWebTrans == false) {
@@ -132,7 +132,7 @@ function buildSearchResult() {
 	retphrase = '';
 	webtrans = '';
 	basetrans = '';
-	_word = '';
+	// _word = '';
 	langType = '';
 	noBaseTrans = false;
 	noWebTrans = false;
@@ -193,7 +193,7 @@ function changeIcon() {
 }
 
 function check() {
-	var word = document.getElementsByName("word")[0].value;
+	var word = document.querySelector("#word").value;
 	window.open("http://dict.youdao.com/search?q=" + encodeURI(word) + "&ue=utf8&keyfrom=chrome.index.g8up");
 }
 /**
@@ -317,3 +317,9 @@ document.querySelector('#backup').onclick = function() {
 		document.body.classList.add('popup');
 	}
 })();
+
+document.body.addEventListener('click', function(e){
+	if( e.target.dataset.toggle === 'play'){
+		playAudio(_word);
+	}
+})
