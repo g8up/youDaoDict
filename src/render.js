f981f5928bc1e5199fbc3cb1f1ed3b5854b80333
g8up <softor@qq.com>
2018-09-29 01:58:38 +0800

    fix: 模板渲染

--- src/render.js
+++ src/render.js
@@ -28,7 +28,7 @@ export const table = (word, speach, phonetic = '', hasBaseTrans, hasWebTrans, ba
           <div class="yddTopBorderlr">
             <a class="yddKeyTitle" href="${searchUrl}" target="_blank" title="查看完整释义">${word}</a>
             ${phonetic ? `<span class="ydd-phonetic">[${phonetic}]</span>` : ''}
-            <span class="ydd-voice">${speach}</span>
+            ${speach ? `<span class="ydd-voice">${speach}</span>` : ''}
             <a class="ydd-detail" href="${searchUrl}" target="_blank">详细</a>
             <a class="ydd-detail" href="javascript:void(0);" id="addToNote" title="添加到单词本">+</a>
             <a class="ydd-close" href="javascript:void(0);">&times;</a>
@@ -39,7 +39,7 @@ export const table = (word, speach, phonetic = '', hasBaseTrans, hasWebTrans, ba
     fmt += renderTransDetail('基本翻译', baseTrans);
   }
   if (hasWebTrans) {
-    renderTransDetail('网络释义', webTrans);
+    fmt += renderTransDetail('网络释义', webTrans);
   } else if (!hasBaseTrans && !hasWebTrans) {
     fmt += `&nbsp;&nbsp;没有英汉互译结果<br/>&nbsp;&nbsp;<a href="${searchUrl}" target=_blank>请尝试网页搜索</a>`;
   }
