5c7401111fa835688b26c972bcaa96e71e03b2be
g8up <g8up@qq.com>
2018-10-06 10:54:30 +0800

    pretty

--- src/util/chrome-sync.js
+++ src/util/chrome-sync.js
@@ -1,26 +1,22 @@
 // https://developer.chrome.com/extensions/storage
 
-const set = ( name, value ) =>{
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.set({ [name]: value },  () => {
-      resolve();
-    });
-  }).catch(err=>{
-    console.warn( err );
+const set = (name, value) => new Promise((resolve, reject) => {
+  chrome.storage.sync.set({ [name]: value }, () => {
+    resolve();
   });
-}
+}).catch((err) => {
+  console.warn(err);
+});
 
-const get = (name) => {
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.get( name,  (ret) =>{
-      resolve(ret); // 未获取到对应值，会返回 {}
-    });
-  }).catch(err => {
-    console.warn(err);
+const get = name => new Promise((resolve, reject) => {
+  chrome.storage.sync.get(name, (ret) => {
+    resolve(ret); // 未获取到对应值，会返回 {}
   });
-}
+}).catch((err) => {
+  console.warn(err);
+});
 
 export default{
   set,
   get,
-}
+};
