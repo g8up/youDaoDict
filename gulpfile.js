e738270c75992c9cf807fb4df14cf7dd467592b7
g8up <softor@qq.com>
2019-03-27 00:49:44 +0800

    simplify package

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const gulp = require('gulp');
 const header = require('gulp-header');
 const less = require('gulp-less');
@@ -6,11 +5,6 @@ const cssmin = require('gulp-cssmin');
 const zip = require('gulp-zip');
 const del = require('del');
 
-const rollup = require('rollup');
-const commonjs = require('rollup-plugin-commonjs');
-const rollupBabel = require('rollup-plugin-babel');
-const rollupUglify = require('rollup-plugin-uglify');
-const { minify } = require('uglify-es');
 const manifest = require('./src/manifest.json');
 const pkg = require('./package.json');
 
@@ -70,95 +64,14 @@ const watch = gulp.series(lessIt, copy, () => {
   gulp.watch(Asset.static, copy);
 });
 
-const getRollupOption = ({ input, dist }) => {
-  const jsBanner = `
-/**
- * ${manifest.name} - ${pkg.description}
- * @version v${VERSION}
- * @author ${pkg.author}
- * @date ${new Date()}
- */`.trim();
-  return {
-    read: {
-      input,
-      output: {
-        globals: {
-          // chrome: 'chrome'
-        },
-      },
-      plugins: [
-        rollupBabel({
-          exclude: 'node_modules/**',
-          include: 'src/**.js',
-        }),
-        commonjs(),
-        rollupUglify({
-          output: {
-            preamble: jsBanner,
-          },
-        }, minify),
-      ],
-      watch: {
-        chokidar: true,
-        include: 'src/**',
-      },
-    },
-    write: {
-      file: dist,
-      format: 'cjs',
-      banner: jsBanner,
-      // sourcemap: true,
-    },
-  };
-};
-
-const compile = (opt) => {
-  const config = getRollupOption(opt);
-
-  rollup.rollup(config.read).then((bundle) => {
-    bundle.write(config.write);
-  }, (err) => {
-    console.log(err);
-  }).catch(err => console.log(err));
-};
-
-const opts = [{
-  input: 'src/background.js',
-  dist: 'dist/background.js',
-}, {
-  input: 'src/options.js',
-  dist: 'dist/options.js',
-}, {
-  input: 'src/lookup.js',
-  dist: 'dist/lookup.js',
-},
-];
-
-const rollupIt = (cb) => {
-  opts.forEach(compile);
-  cb();
-};
-
-const rollupWatch = gulp.series(rollupIt, () => {
-  gulp.watch('./src/**/*.js', (event) => {
-    const dir = event.path;
-    const entry = opts.filter(item => path.resolve(item.input) === dir);
-    if (entry.length) {
-      console.log(entry);
-      entry.forEach(compile);
-    }
-  });
-});
-
-gulp.task('js', gulp.series(rollupIt));// build only js
 gulp.task('less', gulp.series(lessIt));// build only less
-// watcher: js/less/static
-gulp.task('dev', gulp.parallel('js', gulp.parallel(watch, rollupWatch)));
+// watcher: less/static
+gulp.task('watch', gulp.series(watch));
 // build once
-gulp.task('default', gulp.parallel('js', gulp.series('less', copy)));
+gulp.task('static', gulp.series('less', copy));
 // only zip files
 gulp.task('zip', gulp.series(cleanZip, () => gulp.src(`${Dist}**/*`)
   .pipe(zip(zipFile))
   .pipe(gulp.dest(Release))));
 // build + zip
-gulp.task('release', gulp.series('default', 'zip'));// 生成发布到 Chrome Web Store 的 zip 文件
+gulp.task('release', gulp.series('static', 'zip'));// 生成发布到 Chrome Web Store 的 zip 文件
