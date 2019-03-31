e22adbd27c534a3257cf2d2e523521d861bb43ff
g8up <softor@qq.com>
2019-03-31 08:55:01 +0800

    test: 添加用例

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,4 +1,10 @@
 import * as util from '../src/common/util';
+
 test('计算文本中的空格', ()=>{
   expect(util.spaceCount('a b c')).toBe(2);
-})
+  expect(util.spaceCount(' a b c ')).toBe(4);
+});
+
+test('计算文本中的中文个数是否大于5个', ()=>{
+  expect(util.isContainChinese('谢朝平的《大迁徙》，记录了三门峡移民的历史遗留问题，渭南地 区的移民正是作品主角。')).toBe(true);
+});
