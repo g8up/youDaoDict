9194ed5293e81acabf152d3569f3f2d211faee97
g8up <g8up@qq.com>
2019-05-08 18:10:27 +0800

    feat: 插入标签不关联版本特征

--- src/common/shadow-dom.ts
+++ src/common/shadow-dom.ts
@@ -8,7 +8,7 @@ const STYLE_CONTENT = 'style/content.css';
  */
 const markTagOrigin = (tag) => {
   if (tag) {
-    tag.setAttribute('data-comment', '这是 “有道词典划词扩展2018” 插入的节点');
+    tag.setAttribute('data-comment', '这是【有道词典划词扩展】插入的节点');
   }
 };
 
