203145b204af35217e67af914d1e4295d6544a9c
g8up <softor@qq.com>
2019-05-12 21:30:55 +0800

    feat: add promisify()

---
+++ src/common/promisify.ts
@@ -0,0 +1,14 @@
+const promisify = (api, context = null) => (...args) => new Promise((resolve, reject) => {
+  api.apply(context, args.concat((...argus) => {
+    const err = chrome.runtime.lastError;
+    if (err) {
+      reject(err);
+    } else {
+      resolve(...argus);
+    }
+  }));
+}).catch(() => {
+  // log(err);
+});
+
+export default promisify;
--- src/model/Tab.ts
+++ src/model/Tab.ts
@@ -1,6 +1,24 @@
 import {
   WORD_LIST,
 } from '../common/config';
+import ChromeApi from '../common/chrome-api';
+
+/**
+ * 将配置更新通知已经打开的 Tab
+ */
+const publishSettingToTabs = async (options) => {
+  const tabs = await ChromeApi.tabs.query({
+    status: 'complete',
+  });
+
+  if (tabs.length) {
+    return Promise.all(tabs.map((tab) => {
+      return ChromeApi.tabs.sendMessage(tab.id, {
+        optionChanged: options,
+      });
+    }));
+  }
+};
 
 // 打开单词本页面
 export const openWordList = () => {
@@ -10,5 +28,6 @@ export const openWordList = () => {
 };
 
 export default {
+  publishSettingToTabs,
   openWordList,
 }
