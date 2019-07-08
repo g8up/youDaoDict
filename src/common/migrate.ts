0ef8a42f50189677802cd7a66be717806e288353
g8up <g8up@qq.com>
2019-07-08 14:31:02 +0800

    feat：存储逻辑优化

--- src/common/migrate.ts
+++ src/common/migrate.ts
@@ -7,7 +7,7 @@ import History from '../model/History';
 const MIGRATION_STORE_KEY = 'migrated';
 
 export default ()=>{
-  if (localStorage.getItem(MIGRATION_STORE_KEY) === null ) {
+  if (localStorage.getItem(MIGRATION_STORE_KEY) === null ) { // null ==> v1
     const preCache = localStorage.getItem('wordcache');
     if( preCache ) {
       History.cover(preCache.split(',').map(word=>{
@@ -15,7 +15,7 @@ export default ()=>{
           word,
         };
       }));
-      localStorage.setItem(MIGRATION_STORE_KEY, '1'); // mark done
     }
+    localStorage.setItem(MIGRATION_STORE_KEY, 'v1'); // mark done
   }
 }
